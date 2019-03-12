fec534129093bab06842a79767583e7946b36cc1
Karen Arenilla <karenarenilla3@gmail.com>
2019-03-12 17:13:55 -0500

    On branch feature/alquiler
     Changes to be committed:Actualizado
    	modified:   main/contrib/interfaces/controller.ts
    	new file:   main/controllers/alquiler.ts
    	modified:   main/controllers/movie.ts
    	modified:   main/controllers/user.ts
    	modified:   main/models/mongo/alquiler.ts
    	new file:   main/routes/alquiler.ts
    	new file:   main/routes/movie.ts

--- main/controllers/user.ts
+++ main/controllers/user.ts
@@ -4,6 +4,11 @@ import { UserModel } from "models/mongo/user"
 
 export default class implements Controller {
 
+    public async findOne(req: Request, res: Response): Promise<Response> {
+        const user = await UserModel.findOne(req.body);
+        return res.send(user);
+    }
+
     public async findAll(req: Request, res: Response): Promise<Response> {
         const users = await UserModel.find();
         return res.send(users);
@@ -13,5 +18,20 @@ export default class implements Controller {
         const user = new UserModel(req.body);
         await user.save();    
         return res.send(user);
+    }
+
+    public async update(req: Request, res: Response): Promise<Response> {
+        const document = req.body.document;
+        const user = {
+            name: req.body.name,
+            roll: req.body.roll
+        }
+        await UserModel.findOneAndUpdate(document, `${user}`, {new: true});
+        return res.send("update");
+    }
+
+    public async delete(req: Request, res: Response): Promise<Response> {
+        await UserModel.findByIdAndDelete(req.body) 
+        return res.send("delete");
     }
 }
