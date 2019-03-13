b87af5301bac7a5a05744e1d0421c6513efe9450
Karen Arenilla <karenarenilla3@gmail.com>
2019-03-12 20:32:20 -0500

    On branch feature/alquiler
     Changes to be committed:Actualizado
    	modified:   main/models/mongo/alquiler.ts
    	modified:   main/models/mongo/movie.ts
    	modified:   main/models/mongo/user.ts
    	modified:   main/routes/allRoutes.ts

--- main/routes/allRoutes.ts
+++ main/routes/allRoutes.ts
@@ -1,11 +1,18 @@
 import { Application } from "express";
 import user from "./user";
 import userController from "controllers/user"
+import movie from "./movie"
+import movieController from "controllers/movie"
+import alquiler from "./alquiler";
+import alquilerController from "controllers/alquiler"
 
 export default class {
     private version: string;
     constructor(app: Application) {
       this.version = "/api/v1";
       new user(this.version, app, new userController());
+      new movie(this.version, app, new movieController());
+      new alquiler(this.version, app, new alquilerController());
+
     }
 }
