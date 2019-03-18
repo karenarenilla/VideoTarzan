332dcded7094ce2f4719f7f82dfc2f6b98730e21
Karen Arenilla <karenarenilla3@gmail.com>
2019-03-18 13:07:35 -0500

    On branch release: Actualizado
     Changes to be committed:
    	new file:   main/contrib/middleware/auth.ts
    	modified:   main/models/mongo/user.ts
    	modified:   package.json

--- main/models/mongo/user.ts
+++ main/models/mongo/user.ts
@@ -3,8 +3,10 @@ import { prop, Typegoose } from "typegoose"
 export default class UserSchema extends Typegoose {
 
     @prop({required: true, unique: true, index: true}) document?: string;
-    @prop() name?: string;
-    @prop() roll?: string;
+    @prop({required: true}) name?: string;
+    @prop({required: true}) email?: string;
+    @prop({required: true}) password?: string;
+    @prop({required: true}) roll?: string;
 }
 
 export const UserModel = new UserSchema().getModelForClass(UserSchema);
